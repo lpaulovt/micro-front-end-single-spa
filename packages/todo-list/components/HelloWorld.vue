177f5d466aac98b250ab6276b1046dc5e7906075
lpaulovt <silvavitorlopes711@gmail.com>
2021-06-08 15:16:53 -0300

    :sparkles: First commit

---
+++ packages/todo-list/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form @submit.prevent="add()">
+    <input v-model="task" type="text">
+    <button type="submit">Adicionar</button>
+    </form>
+    <ul> 
+      <li v-for="task in tasks" :key="task">{{task}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      task: '',
+      tasks: [],
+    }
+  },
+  methods: {
+    add() {
+      if(this.task.trim()) {
+        this.tasks.push(this.task);
+        this.task = '';
+      }
+    }
+  } 
+}
+</script>
+
